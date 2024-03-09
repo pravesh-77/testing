5aeecb3520e1a2a930040ce7b27aee025cf5a176
Pravesh Parashar <156897162+pravesh-77@users.noreply.github.com>
2024-03-09 15:56:59 +0530

    Update HelloWorld.java

--- src/main/java/hello/HelloWorld.java
+++ src/main/java/hello/HelloWorld.java
@@ -1,10 +1,10 @@
 package hello;
 
-import java.time.LocalTime; // Import LocalTime from java.time
+import java.time.LocalTime;
 
 public class HelloWorld {
     public static void main(String[] args) {
-        LocalTime currentTime = LocalTime.now(); // Use LocalTime.now() to get the current time
+        LocalTime currentTime = LocalTime.now();
         System.out.println("The current local time is: " + currentTime);
         Greeter greeter = new Greeter();
         System.out.println(greeter.sayHello());
